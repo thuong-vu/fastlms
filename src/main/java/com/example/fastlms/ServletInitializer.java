e50797b42c437c767ea7ef6859ff9286dadd6724
Vu Thi Thuong <thuong.vut@gmail.com>
2022-04-28 19:02:01 +0900

    fastlms initial

--- src/main/java/com/example/fastlms/ServletInitializer.java
+++ src/main/java/com/example/fastlms/ServletInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+    //2022.04.28
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
